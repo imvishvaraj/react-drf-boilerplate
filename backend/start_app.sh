0b684cc4693c1612c90f2ed18ee7b92694ba7e5c
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2024-07-03 22:58:06 +0530

    updated env

--- backend/start_app.sh
+++ backend/start_app.sh
@@ -3,8 +3,8 @@
 if [ "$DJANGO_DEBUG" = "true" ]
 then
     echo "Running Django in debug mode"
-    python -Xfrozen_modules=off -m debugpy --listen 0.0.0.0:5678 manage.py runserver 0.0.0.0:8000
+    python -Xfrozen_modules=off -m debugpy --listen 0.0.0.0:${BACKEND_DEBUG_PORT:-5678} manage.py runserver 0.0.0.0:${BACKEND_PORT:-8000}
 else
     echo "Running Django in production mode"
-    gunicorn core.wsgi:application --bind 0.0.0.0:8000
+    gunicorn core.wsgi:application --bind 0.0.0.0:${BACKEND_PORT:-8000}
 fi
