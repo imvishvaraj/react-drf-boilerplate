984b9ed48527427c8db6df9be274979df7847c5f
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2024-07-04 10:00:43 +0530

    updated frontend

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -4,6 +4,6 @@ console.log("FRONTEND_API_BASE_URL: ", process.env.FRONTEND_API_BASE_URL)
 
 export const SETTINGS = {
     "SITE_NAME": "React DRF",
-    "API_BASE_URL": "https://react-drf-ui.apps.vishvaraj.me"
+    "API_BASE_URL": "https://drf-auth-demo.apps.vishvaraj.me"
 }
 
