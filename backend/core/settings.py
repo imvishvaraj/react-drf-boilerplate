77e7c46762c0254b8d771b652f05594dc550ca24
Vishvaraj Dhanawade <41696196+imvishvaraj@users.noreply.github.com>
2024-07-21 22:37:36 +0530

    added cors methods headers

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -175,4 +175,31 @@ SIMPLE_JWT = {
 #     "https://react-drf-ui.apps.vishvaraj.me",
 #     "https://*.apps.vishvaraj.me"
 # ]
-CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://react-drf-ui.apps.vishvaraj.me",
+    "https://drf-auth-demo.apps.vishvaraj.me/",
+]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
